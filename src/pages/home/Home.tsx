6214a5b1711d66891edf815225fd53b70fbdd459
KiBohr <katharinabohr@web.de>
2025-06-10 13:15:52 +0200

    deleted irrelevant comment

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -4,7 +4,6 @@ import { PokemonList } from "../../contracts/interfaces";
 import SingleCard from "../../components/singleCard/SingleCard";
 import Loading from "../../components/loading/Loading";
 
-// ! Typisierung?? Was ist hier los??
 
 const Home = () => {
 	const { pokemons, setPokemons } = useContext(mainContext) as PokeContext;
@@ -21,7 +20,7 @@ const Home = () => {
 				placeholder="type something"
 				className='border-2 rounded-full px-5 py-2 border-white bg-white text-text-outer md:px-20 md:text-2xl lg:px-40 lg:text-3xl'
 			/>
-			
+
 			{!pokemons && <Loading />}
 			<div className='grid grid-cols-2 gap-5 mx-5 my-10 sm:grid-cols-3 md:grid-cols-4 md:mx-10 lg:grid-cols-5 lg:mx-20 lg:gap-10'>
 				{pokemons
